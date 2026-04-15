2a42bd79dd0ca70237e1229bd3a6b0a1c06f028e
VClark24 <6clarv16@solent.ac.uk>
2026-04-15 01:54:16 +0100

    Update mute_multiplex.vhd

--- VHDL/Non-AXI IP Cores/Mute Control/mute_multiplex.vhd
+++ VHDL/Non-AXI IP Cores/Mute Control/mute_multiplex.vhd
@@ -1,4 +1,20 @@
--- 16/03/2026
+-----------------------------------------------------------------------------------------------
+-- VHDL Compatibility: IEEE Std 1076-2008
+-- Organisation: Southampton Solent University
+-- Engineer: Vivienne Clark
+-- Module Name: Mute Multiplexer
+-- Revisions:
+--  V1.0: Created 16/03/2026 in GitHub (https://github.com/VClark24/Project_Transmitter)
+-- Comments:
+--  This block will mute the output in these two following situations:
+-- 
+--  When the user wishes to mute, a register within the Transmit Controller block can be written
+--  to. The mute_o signal is fed into this block. When high the outputs of the mapper are forced
+--  low (0). These outputs are then fed into the SRRC filter
+-- 
+--  During waveform switching, a signal is sent from the Path Selector block to temporarily mute
+--  the transmitter in order to prevent glitching. 
+-----------------------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -9,7 +25,7 @@ entity mute_multiplex is
     clk : in std_logic;
     rst_n : in std_logic;
     
-    mute_sw : in std_logic;  -- Mute from SW
+    mute_sw : in std_logic;  -- Mute from SW (mute_o)
     mute_tmp : in std_logic;  -- Mute due to waveform switching
     
     inphase_i : in std_logic_vector(15 downto 0);
