0a3b82982530232dae780495906811fe2276d428
VClark24 <6clarv16@solent.ac.uk>
2026-04-15 02:02:51 +0100

    Update transmit_mux.vhd

--- VHDL/Non-AXI IP Cores/Path Control/transmit_mux.vhd
+++ VHDL/Non-AXI IP Cores/Path Control/transmit_mux.vhd
@@ -1,5 +1,17 @@
---16/03/2026
-
+-----------------------------------------------------------------------------------------------------------
+-- VHDL Compatibility: IEEE Std 1076-2008
+-- Organisation: Southampton Solent University
+-- Engineer: Vivienne Clark
+-- Module Name: Transmit Multiplexer
+-- Revisions:
+--  V1.0: Created 16/03/2026 in GitHub (https://github.com/VClark24/Project_Transmitter)
+-- Comments:
+--  Essentially rejoins the three paths (telecommand, telemetry and bypassing framing + encoding
+--  If telecommand is selected, the output of the BCH encoder is fed into the mapper
+--  If telemetry is selected, the output of the convolutional encoder is fed into the mapper
+--  If beacon or FHSS is selected, the output of the PRBS23 block is fed straight into the mapper
+--  Takes the path_sel signal (0 for bypass, 1 for telecommand, 2 for telemetry) from the path selecter
+---------------------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
